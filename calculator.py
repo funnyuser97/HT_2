c26b44ea10f78684e835991ba0ba4ab382a192d1
funnyuser97 <32933072+funnyuser97@users.noreply.github.com>
2017-10-31 15:59:49 +0000

    update calc

--- calculator.py
+++ calculator.py
@@ -1,1 +1,18 @@
+def calculate(action,num1,num2):
+    if action=='+':
+        res=num1+num2
+    elif action=='-':
+        res=num1-num2
+    elif action=='*':
+        res=num1*num2
+    elif action=='/':
+        res=num1/num2
+    else: res='error, incorrect choose action'
+    return res
+#forexample calculate('+',2,3) return 5
 
+action=input('Input action: ')
+num1=float(input('Input first number: '))
+num2=float(input('Input second number: '))
+
+print(calculate(action,num1,num2))	
