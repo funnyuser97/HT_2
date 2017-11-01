00427865a02ca3d3857566bfa4d8c6280a520ae4
funnyuser97 <32933072+funnyuser97@users.noreply.github.com>
2017-11-01 15:19:29 +0200

    Update compare.py

--- compare.py
+++ compare.py
@@ -1,12 +1,12 @@
 def compare(x,y):
     if x>y:
-        print('x biger than y on ',x-y)
+        print('x bigger than y on ',x-y)
     elif x<y:
-        print('y biger than x on ',y-x)
+        print('y bigger than x on ',y-x)
     else:
         print('x equal  y ')
     return;
-#for example: compare(23,12) - output string 'x biger than y on 11'
+#for example: compare(23,12) - output string 'x bigger than y on 11'
 x=int(input('input x:'))  
 y=int(input('input y:')) 
 compare(x,y)
